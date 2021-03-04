3a43c2b2c1077265ffdf38486914a207bcb46040
courange <courange.c@gmail.com>
2021-03-04 09:06:05 +0900

    最適化

--- DoZipping/Program.cs
+++ DoZipping/Program.cs
@@ -1,7 +1,6 @@
 ﻿namespace Zipper {
     class Program {
         static void Main(string[] args) {
-            Logger log = new Logger();
             new AppConfig();
             Process.MainProcess(args);
         }
